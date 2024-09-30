69f52b1a79251e58efa79d98d25dd862130af8d2
Eduardo <eduardo25082002@gmail.com>
2024-09-30 16:34:25 -0300

    Commit Final

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -4,6 +4,8 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner bancaria = new Scanner(System.in);
         
+    
+
         int numero;
         String agencia;
         String nomeCliente;
@@ -21,8 +23,9 @@ public class ContaTerminal {
         System.out.println("O seu saldo é: ");
         saldo = bancaria.nextDouble();
 
-        
+        System.out.println("Olá\n" + nomeCliente + "\nobrigado pro criar uma conta em nosso banco, sua agência é:\n " + agencia + "\nconta:\n " + numero + "\ne seu saldo é:\n " + saldo + "\njá está disponivel para saque!");
     
+        bancaria.close();
     }
-    
+
 }
